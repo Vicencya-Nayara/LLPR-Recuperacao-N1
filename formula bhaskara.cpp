bc0849bf7840f05a338ae92de81d4e6c5435e3a6
Vicencya-Nayara <115300537+Vicencya-Nayara@users.noreply.github.com>
2022-11-04 00:28:05 -0300

    Update formula bhaskara.cpp

--- formula bhaskara.cpp
+++ formula bhaskara.cpp
@@ -1,10 +1,7 @@
 #include <stdio.h>
 #include <math.h>
-#include <locale.h>
  
 int main() {
-setlocale(LC_ALL, "Portuguese_Brazil");
-	
     double A, B, C;
  
     printf("Digite o primeiro numero: ");
@@ -21,16 +18,10 @@ setlocale(LC_ALL, "Portuguese_Brazil");
     printf("Impossivel calcular\n");
    }
  
-   else{	
-    printf("Impossivel calcular\n");
-    }
- 
-    if((B*B - 4*A*C) < 0 || (A == 0))
-  {
+    else{
     printf("R1 = %.5lf\n",(-B + sqrt(B*B -4*A*C))/(2*A));
     printf("R2 = %.5lf\n",(-B - sqrt(B*B -4*A*C))/(2*A));
-  }
- 
+	}
  
     return 0;
 }
